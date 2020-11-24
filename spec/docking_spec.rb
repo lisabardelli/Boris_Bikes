0e71421c6f951964b6d2847516638e0e2537a975
Simon Piper <simon.a.piper87@gmail.com>
2020-11-24 21:35:02 +0000

    Simon 2nd commit - step 10 complete - all feature and unit tests pass - good to go

--- spec/docking_spec.rb
+++ spec/docking_spec.rb
@@ -2,19 +2,9 @@ require "docking"
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-  #'should respond to :release_bike' do
-    #expect(DockingStation).to respond_to(:release_bike)
-  # it 'is of class DockingStation' do
-  #   expect(docking_station).to be_instance_of DockingStation
-  # end
-
-  #it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
 end
-end
-
-  #it 'will release_bike when passed "release_bike"' do
-    #expect(DockingStation.new).to respond_to(:release_bike)
