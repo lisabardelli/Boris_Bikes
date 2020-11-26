3bccbedf5513247a3a3a14122de4b9d7c1a848ea
Sujeevan Rajagopalan <rsujee@MA307.local>
2020-11-26 14:43:38 +0000

    using instance variables

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,16 @@
 require './lib/bike.rb'
 
 class DockingStation 
+  attr_reader :station
+  def initialize
+    @station = []
+  end
+
   def release_bike
     Bike.new
+  end
+
+  def dock
+    @station.push(Bike.new)
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,8 +4,15 @@ describe DockingStation do
   it { is_expected.to respond_to(:release_bike)} 
   
   it 'gets a bike' do
+    subject = DockingStation.new
     expect(subject.release_bike).to be_instance_of Bike
   end 
+
+  it 'docking station allows a bike to be docked' do
+    expect(subject).to respond_to :dock
+  end
+
+  
 end
 
 
