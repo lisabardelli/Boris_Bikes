5de45df3708054dea0b622ee10033b05c517bf5f
Chris Abdo <christophermabdo@icloud.com>
2020-11-26 16:35:13 +0000

    Chris commits 26/11

--- lib/docking.rb
+++ lib/docking.rb
@@ -3,13 +3,25 @@ require_relative 'bike'
 class DockingStation
  attr_reader :bike
 
+  def initialize
+    @aval_bikes = []
+  end
 
   def dock(bike)
     @bike = bike
+    if @aval_bikes.length >= 1
+      raise StandardError.new "Docking station full"
+    else
+      @aval_bikes << bike
+    end
   end
 
   def release_bike
-    Bike.new
+    if @aval_bikes.length > 0
+      Bike.new
+    else
+      raise StandardError.new "There is no bike"
+    end
   end
 
 end
--- spec/docking_spec.rb
+++ spec/docking_spec.rb
@@ -1,12 +1,18 @@
 require "docking"
+require "bike"
 
 describe DockingStation do
+
   it { is_expected.to respond_to :release_bike }
+
 
   it 'docks something' do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq [bike]
   end
+
+  bike = Bike.new
+  it { expect{docking_station.dock(bike_2)}.to raise_error StandardError}
 
   it 'returns docked bikes' do
     bike = Bike.new
@@ -14,8 +20,17 @@ describe DockingStation do
     expect(subject.bike).to eq bike
   end
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  #it 'releases working bikes' do
+  #  bike = subject.release_bike
+  #  expect(bike).to be_working
+  # end
+  it 'should release a bike' do
+    bike = Bike.new
+    expect(subject.release_bike).to be_an_instance_of Bike
   end
+
+let(:aval_bikes) {[]}
+bike = Bike.new
+  it { expect{docking_station.release_bike}.to raise_error StandardError}
+
 end
