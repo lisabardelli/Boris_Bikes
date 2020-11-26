f481cb911d1fb7fda616ddd5922d3cfb25502c1a
Chris Abdo <christophermabdo@icloud.com>
2020-11-26 14:25:45 +0000

    Chris commit step 11

--- spec/docking_spec.rb
+++ spec/docking_spec.rb
@@ -3,6 +3,17 @@ require "docking"
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
